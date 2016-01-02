61e58b8f919e7070390a7504691a16073de69c0d
Ben! <ben@bensites.com>
2016-01-02 15:47:32 -0600

    Custom Crafting & Jetpack example.

--- examples/custom_crafting.rb
+++ examples/custom_crafting.rb
@@ -3,7 +3,7 @@ require 'diamond-lang'
 class CustomCrafting < DiamondLang::OneCommand
   def setup(c)
     c.title s(:a), :subtitle, {"text " => "command by Ben from bensites.com"}.to_json
-    c.title s(:a), :title, {"text " => "Custom"}.to_json
+    c.title s(:a), :title, {"text " => "Custom Crafting"}.to_json
     add_crafting([
       {
         pattern:  "  s"+
@@ -12,9 +12,9 @@ class CustomCrafting < DiamondLang::OneCommand
         result: "   "+
                 " n "+
                 "   ",
-        n: "id:\"minecraft:name_tag\",Count:1b",
-        s: "id:\"minecraft:string\",Count:1b",
-        p: "id:\"minecraft:paper\",Count:1b"
+        n: 'id:"minecraft:name_tag",Count:1b',
+        s: 'id:"minecraft:string",Count:1b',
+        p: 'id:"minecraft:paper",Count:1b'
       },
       {
         pattern:  "bbb"+
@@ -22,9 +22,9 @@ class CustomCrafting < DiamondLang::OneCommand
                   "bbb",
         result: "   "+
                 " s ",
-        b: "id:\"minecraft:iron_bars\",Count:1b",
-        p: "id:\"minecraft:porkchop\",Count:64b",
-        s: "id:\"minecraft:mob_spawner\",Count:1b"
+        b: 'id:"minecraft:iron_bars",Count:1b',
+        p: 'id:"minecraft:porkchop",Count:64b',
+        s: 'id:"minecraft:mob_spawner",Count:1b'
       }
     ])
   end
---
+++ examples/jetpack.rb
@@ -0,0 +1,32 @@
+require 'diamond-lang'
+
+class CustomCrafting < DiamondLang::OneCommand
+  def setup(c)
+    c.title s(:a), :subtitle, {"text " => "command by Ben from bensites.com"}.to_json
+    c.title s(:a), :title, {"text " => "Jetpack"}.to_json
+    @jetpack = 'id:"minecraft:elytra",Count:1b,tag:{display:{Name:Jetpack,Lore:[A jetpack that,lets you fly!]}}'
+    add_crafting([
+      {
+        pattern:  "rrr"+
+                  "rer"+
+                  "fff",
+        result: "   "+
+                " j "+
+                "   ",
+        r: 'id:"minecraft:redstone",Count:1b',
+        e: 'id:"minecraft:elytra",Count:1b',
+        f: 'id:"minecraft:fire_charge",Count:1b',
+        j: @jetpack
+      }
+    ])
+    c.scoreboard :objectives, :add, :jetpack, :dummy, "Is holding a jetpack."
+  end
+
+  def tick(c)
+    c.scoreboard :players, :set, s(:a), :jetpack, 0
+    c.scoreboard :players, :set, s(:a), :jetpack, 1, "{SelectedItem:{#{@jetpack}}}"
+    c.effect s(:a, {score_jetpack_min: 1}), "minecraft:levitation", 1, 10
+  end
+end
+
+CustomCrafting.create
