787629687eae6e3f647ba5ea94ea4bddb72d1a8d
Ben! <ben@bensites.com>
2016-01-03 12:07:48 -0600

    Jetpack is 100% more awesome now.

--- examples/jetpack.rb
+++ examples/jetpack.rb
@@ -19,13 +19,43 @@ class Jetpack < DiamondLang::OneCommand
         j: @jetpack
       }
     ])
-    c.scoreboard :objectives, :add, :jetpack, :dummy, "Is holding a jetpack."
+    c.scoreboard :objectives, :add, :jetpack, :dummy, "Holding Jetpack"
+    c.scoreboard :objectives, :add, :sneaking, "stat.sneakTime", "Sneaking"
+    c.scoreboard :objectives, :add, :jetpack_speed, :dummy, "Jetpack Speed"
   end
 
   def tick(c)
+    not_sneaking = s(:a, {score_jetpack_min: 1, score_sneaking: 0})
+    sneaking = s(:a, {score_jetpack_min: 1, score_sneaking_min: 1})
+
     c.scoreboard :players, :set, s(:a), :jetpack, 0
-    c.scoreboard :players, :set, s(:a), :jetpack, 1, "{SelectedItem:{#{@jetpack}}}"
-    c.effect s(:a, {score_jetpack_min: 1}), "minecraft:levitation", 1, 10
+    c.scoreboard :players, :set, s(:a), :jetpack, 1, "{Inventory:[{#{@jetpack},Slot:102b}]}"
+    jetpack_players = s(:a, {score_jetpack_min: 1})
+
+    c.scoreboard :players, :add, sneaking, :jetpack_speed, 1
+    c.scoreboard :players, :set, not_sneaking, :jetpack_speed, 0
+
+    {
+      0 => 0,
+      1 => 5,
+      5 => 7,
+      10 => 10,
+      20 => 15,
+      30 => 25,
+      40 => 30,
+      50 => 35,
+      60 => 40,
+      70 => 45,
+      80 => 46,
+      90 => 47,
+      100 => 48,
+      110 => 49,
+      120 => 50
+    }.each do |time, level|
+      c.effect s(:a, {score_jetpack_min: 1, score_jetpack_speed_min: time}), "minecraft:levitation", level == 0 ? 0 : 1, level
+    end
+
+    c.scoreboard :players, :set, s(:a), :sneaking, 0
   end
 end
 
